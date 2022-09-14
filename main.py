315f35c889461b3e95ef93b331a4461c330261b5
LinuxGamer <charl.cj.monke@gmail.com>
2022-09-14 12:48:55 +0100

    started work on Terminal Emulator

--- main.py
+++ main.py
@@ -67,6 +67,10 @@ class MyWindow1(Gtk.Window):
         launcher.set_hexpand(True)
         launcher.connect("clicked", self.on_launcher_clicked)
 
+        terminal = Gtk.Button(label="Terminal")
+        terminal.set_hexpand(True)
+        terminal.connect("clicked", self.on_terminal_clicked)
+
         button7 = Gtk.Button(label="Exit")
         button7.set_hexpand(True)
         button7.connect("clicked", Gtk.main_quit)
@@ -78,12 +82,20 @@ class MyWindow1(Gtk.Window):
         grid1.attach(button1, 0, 7, 1, 1)
         grid1.attach(button2, 1, 7, 1, 1)
         grid1.attach(button3, 2, 7, 1, 1)
-        grid1.attach(launcher, 1, 16, 1, 1)
+        grid1.attach(launcher, 0, 16, 1, 1)
+        grid1.attach(terminal, 1 ,16, 1, 1)
         grid1.attach(button7, 2, 30, 1, 1)
         grid1.attach(label4,  0, 15, 3, 1)
 
         self.add(frame1)
         frame1.add(grid1)
+
+    def on_terminal_clicked(self, widget):
+        print("User chose: Launch Terminal")
+        subprocess.run(
+        win1.hide(),
+        win3.show_all())
+        
 
     def on_button1_clicked(self, widget):
         print("User chose: About Universe")
@@ -185,6 +197,10 @@ class MyWindow2(Gtk.Window):
         dolphin.set_hexpand(True)
         dolphin.connect("clicked", self.on_dolphin_clicked)
 
+        gimp = Gtk.Button(label="GIMP")
+        gimp.set_hexpand(True)
+        gimp.connect("clicked",self.on_gimp_clicked)
+
         button20 = Gtk.Button(label="Back To Main Menu")
         button20.set_hexpand(True)
         button20.connect("clicked", self.on_button20_clicked)
@@ -196,14 +212,15 @@ class MyWindow2(Gtk.Window):
         #grid2.attach(image1,   0, 0, 4, 2)
         grid2.attach(label1,   0, 2, 4, 2)
         grid2.attach(label2,   0, 4, 4, 2)
-        grid2.attach(firefox,  3, 6, 1, 1)
+        grid2.attach(firefox,  0, 7, 1, 1)
         grid2.attach(alacritty,  0, 6, 1, 1)
-        grid2.attach(code, 3, 7, 1, 1)
+        grid2.attach(code, 0, 8, 1, 1)
         grid2.attach(blender, 1, 6, 1, 1)
-        grid2.attach(writer, 1, 7, 1, 1)
-        grid2.attach(nitrogen, 2, 7, 1, 1)
+        grid2.attach(writer, 2, 7, 1, 1)
+        grid2.attach(nitrogen, 3, 7, 1, 1)
         grid2.attach(emacs, 2, 6, 1, 1)
-        grid2.attach(dolphin, 0, 7, 1, 1)
+        grid2.attach(dolphin, 3, 6, 1, 1)
+        grid2.attach(gimp, 1, 7, 1, 1)
         grid2.attach(label3,   0, 9, 4, 1)
         grid2.attach(button20, 0, 10, 2, 1)
         grid2.attach(button21, 2, 10, 2, 1)
@@ -242,6 +259,10 @@ class MyWindow2(Gtk.Window):
     def on_dolphin_clicked(self, widget):
         print("Launcher: Dolphin")
         subprocess.run(["dolphin"])
+    
+    def on_gimp_clicked(self, widget):
+        print("Launcher: GIMP")
+        subprocess.run(["gimp"])
 
     def on_button20_clicked(self, widget):
         print("Back To Main Menu")
@@ -252,12 +273,31 @@ class MyWindow2(Gtk.Window):
         print("Exit")
         button21.connect("clicked", Gtk.main_quit)
 
+# Window 3 - App Launcher
+class MyWindow3(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Universe: Terminal Emulator")
+
+        self.set_border_width(10)
+        self.set_default_size(640, 300)
+        self.set_position(Gtk.WindowPosition.CENTER)
+        self.set_resizable(False)
+
+        frame3 = Gtk.Frame(label="Terminal Emulator")
+
+        grid3 = Gtk.Grid(row_spacing    = 10,
+                         column_spacing = 10,
+                         column_homogeneous = True)
+       
+
 
 win1 = MyWindow1()
 win2 = MyWindow2()
+win3 = MyWindow3()
 
 win1.connect("destroy", Gtk.main_quit)
 win2.connect("destroy", Gtk.main_quit)
+win3.connect("destroy", Gtk.main_quit)
 
 
 win1.show_all()
