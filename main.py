79ad1084891f65ac49b5479cbcc8e52727d9e7ca
CJ <charl.cj.monke@gmail.com>
2023-01-15 15:14:57 +0000

    Push from local

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2022 CJ (LinuxGamer)
+
+# This file is part of Universe.
+
+# Universe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Universe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Universe.  If not, see <http://www.gnu.org/licenses/>.
+
+# Author: CJ (LinuxGamer), and others, see contributors list.
+
 from PySide6.QtWidgets import  QApplication, QWidget
 # from rockwidget import RockWidget
 from mainwindow import MainWindow
--- mainwindow.py
+++ mainwindow.py
@@ -1,6 +1,26 @@
+# Copyright (C) 2022 CJ (LinuxGamer)
+
+# This file is part of Universe.
+
+# Universe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Universe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Universe.  If not, see <http://www.gnu.org/licenses/>.
+
+# Author: CJ (LinuxGamer), and others, see contributors list.
+
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import *
+import logging
 
 class MainWindow(QMainWindow):
     def __init__(self, app):
@@ -40,10 +60,12 @@ class MainWindow(QMainWindow):
         if ret == QMessageBox.Ok :
             print("User chose OK")
         else :
-            print ("User chose Cancel")
+            print("User chose Cancel")
 
     def quit_app(self):
         self.app.quit()
+        print("User Quit App")
 
     def toolbar_button_click(self):
         self.statusBar().showMessage("Message from my app")
+    
